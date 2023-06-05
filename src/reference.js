248ab5aec9c87c20041b0fb3da3bb313d854b19e
Julien Veyssier <julien-nc@posteo.net>
2023-06-05 17:51:33 +0200

    do not use OC functions/vars anymore
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -20,9 +20,11 @@
  */
 
 import { registerWidget, registerCustomPickerElement, NcCustomPickerRenderResult } from '@nextcloud/vue/dist/Components/NcRichText.js'
+import { linkTo } from '@nextcloud/router'
+import { getRequestToken } from '@nextcloud/auth'
 
-__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
-__webpack_public_path__ = OC.linkTo('integration_replicate', 'js/') // eslint-disable-line
+__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_public_path__ = linkTo('integration_replicate', 'js/') // eslint-disable-line
 
 registerWidget('integration_replicate_image', async (el, { richObjectType, richObject, accessible }) => {
 	const { default: Vue } = await import(/* webpackChunkName: "vue-lazy" */'vue')
