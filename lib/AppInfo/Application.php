a661931585406da53639460966f78e4428faaed9
Julien Veyssier <julien-nc@posteo.net>
2024-03-25 16:41:16 +0100

    remove pickers
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -9,9 +9,6 @@
 
 namespace OCA\Replicate\AppInfo;
 
-use OCA\Replicate\Listener\ReplicateReferenceListener;
-use OCA\Replicate\Reference\ImageReferenceProvider;
-use OCA\Replicate\Reference\WhisperReferenceProvider;
 use OCA\Replicate\SpeechToText\STTProvider;
 use OCA\Replicate\TextProcessing\FreePromptProvider;
 use OCA\Replicate\TextToImage\TextToImageProvider;
@@ -20,7 +17,6 @@ use OCP\AppFramework\Bootstrap\IBootContext;
 
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\Collaboration\Reference\RenderReferenceEvent;
 use OCP\IConfig;
 
 class Application extends App implements IBootstrap {
@@ -32,25 +28,18 @@ class Application extends App implements IBootstrap {
 	public const DEFAULT_LLM_NAME = 'mistralai/mistral-7b-instruct-v0.1';
 	public const DEFAULT_LLM_VERSION = '83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70';
 
-	public const PROMPT_TYPE_IMAGE = 0;
-	public const MAX_PROMPT_PER_TYPE_PER_USER = 5;
-
 	private IConfig $config;
 
 	public function __construct(array $urlParams = []) {
 		parent::__construct(self::APP_ID, $urlParams);
 
 		$container = $this->getContainer();
-		$this->config = $container->query(IConfig::class);
+		$this->config = $container->get(IConfig::class);
 	}
 
 	public function register(IRegistrationContext $context): void {
 		$apiKey = $this->config->getAppValue(self::APP_ID, 'api_key');
 		if ($apiKey !== '') {
-			$context->registerReferenceProvider(ImageReferenceProvider::class);
-			$context->registerReferenceProvider(WhisperReferenceProvider::class);
-			$context->registerEventListener(RenderReferenceEvent::class, ReplicateReferenceListener::class);
-
 			$context->registerSpeechToTextProvider(STTProvider::class);
 			$context->registerTextToImageProvider(TextToImageProvider::class);
 			$context->registerTextProcessingProvider(FreePromptProvider::class);
